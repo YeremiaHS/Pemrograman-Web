5ebf0922a57eb21da0f2d80c58c68b350adf1a6d
Yeremia Herbert <bukanyeremia@gmail.com>
2022-10-07 07:34:10 +0700

    e-commerce
    Add e-commerce layout to repository

---
+++ Pertemuan-2/ECOMMERCE_YEREMIA/content.php
@@ -0,0 +1,178 @@
+      <!-- Product -->
+      <section id="product">
+      <div class="latest-products">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="section-heading">
+                <h2>Produk Kami</h2>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 1.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>Beyond Good and Evil</h4></a>
+                  <h6>Rp 268.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (24)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 2.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>1984 by George Orwell</h4></a>
+                  <h6>Rp 180.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (21)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 3.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>The Republic</h4></a>
+                  <h6>Rp 180.000</h6>
+                  <p>The Republic by Plato</p>
+                  <br>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (36)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 4.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>Neuromancer by William Gibson</h4></a>
+                  <h6>Rp 230.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (48)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 5.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>No Longer Human</h4></a>
+                  <h6>Rp 190.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (16)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 6.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>The Conquest of Happiness</h4></a>
+                  <h6>Rp 230.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (32)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 8.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>Being and Time</h4></a>
+                  <h6>Rp 290.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (24)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 9.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>The History of Freedom</h4></a>
+                  <h6>Rp 300.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (24)</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="product-item">
+                <a href="#"><img src="assets/image/Frame 10.png" alt=""></a>
+                <div class="down-content">
+                  <a href="#"><h4>Goodbye Eri</h4></a>
+                  <h6>Rp 168.000</h6>
+                  <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                  <ul class="stars">
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                    <li><img src="assets/image/Star 1.png"></img></li>
+                  </ul>
+                  <span>Reviews (24)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      <!-- Product -->
---
+++ Pertemuan-2/ECOMMERCE_YEREMIA/index.php
@@ -0,0 +1,22 @@
+<?php
+    include_once "header.php";
+    include_once "banner.php";
+    include_once "content.php";
+?>
+<!-- Footer -->
+<footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="inner-content">
+                <p>Copyright &copy; 2022 Toko Buku Jaya
+                <a href="https://www.instagram.com/yeremiah_s/">Yeremia Herbert Santoso</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- Footer -->
+    </header>
+  </body>
+</html>
