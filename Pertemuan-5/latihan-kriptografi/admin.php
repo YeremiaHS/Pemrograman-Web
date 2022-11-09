c6d09dcc2648d4c6b8adf2f7fa5e7ec60cb5b601
Yeremia Herbert <bukanyeremia@gmail.com>
2022-11-09 11:25:30 +0700

    Kriptografi

---
+++ Pertemuan-5/latihan-kriptografi/admin.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])) {
+    header('Location:login-page.php');
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <meta hhtp-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Admin Page </title>
+    </head>
+
+    <body>
+        <h1><p align=center> Welcome, <?php $_SESSION['username'];?> </p></h1>
+        <p align = center> Kamu sedang berada di halaman admin </p>
+        <br>
+        <h3><p align=center><a href="logout.php"> Logout </a></p></h3>
+    </body>
+</html>
---
+++ Pertemuan-5/latihan-kriptografi/logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if(isset($_SESSION['username'])) {
+    session_destroy();
+    echo '<script>
+    alert("Anda berhasil logout");
+    window.location="login-page.php";
+    </script>';
+} else {
+    header("location:login-page.php");
+    exit();
+}
+
+?>
