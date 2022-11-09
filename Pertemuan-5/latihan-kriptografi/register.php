75cd0626b3eb433715668dd936c51ae0ab33fcbe
Yeremia Herbert <bukanyeremia@gmail.com>
2022-11-09 13:25:37 +0700

    Kriptografi
    fix 2

--- Pertemuan-5/latihan-kriptografi/register.php
+++ Pertemuan-5/latihan-kriptografi/register.php
@@ -4,58 +4,99 @@ session_start();
 if (isset($_SESSION['username'])) {
     header('Location:admin.php');
 }
+
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Register Page </title>
+    <title>Login</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    <a href="index.php"> Go to Home </a>
-    <br /><br />
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/bg-01.jpg')">
+            <div class="wrap-login100 p-t-30 p-b-50">
+                <span class="login100-form-title p-b-41"> Account Register </span>
+                <form action="register-process.php" method="POST" class="login100-form validate-form p-b-33 p-t-5">
+                    <div class="wrap-input100 validate-input" data-validate="Enter username">
+                        <input class="input100" type="text" name="username" placeholder="Username" />
+                        <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                    </div>
 
-    <form action="register-process.php" method="POST">
-        <table width="25%" border="0">
-            <tr>
-                <td> Username </td>
-                <td> :< </td>
-                <td> <input type="text" name="username"></td>
-            </tr>
-            <tr>
-                <td> Gender </td>
-                <td> :< </td>
-                <td>
-                    <select name="gender" id="gender">
-                        <option value="pria"> Pria </option>
-                        <option value="wanita"> Wanita</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td> Email </td>
-                <td> :< </td>
-                <td> <input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td> Password </td>
-                <td> :< </td>
-                <td> <input type="text" name="password"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td> <input type="submit" name="submit" value="Register"></td>
-            </tr>
-        </table>
-    </form>
+                    <div class="wrap-input100 validate-input" data-validate="Enter email">
+                        <input class="input100" type="text" name="email" placeholder="Email" />
+                        <span class="focus-input100" data-placeholder="&#xe809;"></span>
+                    </div>
 
-    <br><br>
-    Sudah Memiliki Akun? <a href="login-page.php"> Login </a>
+                    <div class="wrap-input100 validate-input" data-validate="Enter gender">
+                        <select class="input100 material-icons face" name="gender" id="gender">
+                            <option value="pria"> Pria </option>
+                            <option value="wanita"> Wanita</option>
+                        </select>
+                        <span class="focus-input100" data-placeholder="	&#xe813;"></span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Password" />
+                        <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                    </div>
+
+                    <div class="container-login100-form-btn m-t-32">
+                        <input type="submit" name="submit" value="Register">
+                    </div>
+
+                    <div class="container-login100-form-btn m-t-32">
+                        Sudah Memiliki akun <a href="login-page.php"> Login </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
